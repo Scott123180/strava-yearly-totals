e0a0b5726ca74c47710e901e6182d10c7a8b2e14
scott123180 <me@scotthansen.io>
2023-12-30 10:44:36 -0500

    added functionality

--- src/main/java/io/scotthansen/strava/Application.java
+++ src/main/java/io/scotthansen/strava/Application.java
@@ -1,46 +1,60 @@
 package io.scotthansen.strava;
 
-import java.io.BufferedReader;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
+import java.math.BigDecimal;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Application {
-    public static void main(String[] args) {
+
+    private static final double KM_TO_MILES_CONVERSION_FACTOR = 1.60934;
+    public static void main(String[] args) throws IOException, ParseException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the year for which to sum the miles:");
-        int year = scanner.nextInt();
+        int inputYear = scanner.nextInt();
+
+        System.out.println("Enter the file path: ");
+        final String csvFile = scanner.next();
         scanner.close();
 
-        String csvFile = "path/to/your/csvfile.csv"; // Replace with the actual file path
-        String line = "";
-        String csvSplitBy = ",";
-        double totalMiles = 0.0;
 
-        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-            while ((line = br.readLine()) != null) {
-                String[] activity = line.split(csvSplitBy);
-                String activityType = activity[3]; // Assuming 'Activity Type' is the fourth column
-                String activityDate = activity[1]; // Assuming 'Activity Date' is the second column
+        BigDecimal totalKilometers = BigDecimal.ZERO;
 
-                // Check if the activity is a Ride or Virtual Ride
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM d, yyyy, h:mm:ss a", Locale.ENGLISH);
+
+        try (Reader reader = new FileReader(csvFile)) {
+            CSVParser parser = new CSVParser(reader, CSVFormat.DEFAULT.withFirstRecordAsHeader());
+
+            for (final CSVRecord record : parser) {
+                String activityType = record.get("Activity Type");
+                String activityDateStr = record.get("Activity Date");
+                String distanceStr = record.get("Distance");
+
                 if (activityType.equals("Ride") || activityType.equals("Virtual Ride")) {
-                    Date date = new SimpleDateFormat("MMM d, yyyy, h:mm:ss a").parse(activityDate);
-                    if (date.getYear() + 1900 == year) {
-                        double distance = Double.parseDouble(activity[6]); // Assuming 'Distance' is the seventh column
-                        totalMiles += distance;
+                    LocalDate activityDate = LocalDate.parse(activityDateStr, formatter);
+                    if (activityDate.getYear() == inputYear) {
+                        BigDecimal distanceKm = new BigDecimal(distanceStr);
+                        totalKilometers = totalKilometers.add(distanceKm);
                     }
+
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
         }
 
-        System.out.println("Total miles in " + year + ": " + totalMiles);
+        System.out.println("Total miles in " + inputYear + ": " + kilometersToMiles(totalKilometers.doubleValue()));
+    }
+
+    private static double kilometersToMiles(double kilometers) {
+        return kilometers / KM_TO_MILES_CONVERSION_FACTOR;
     }
 }
